ac1ef243c3f98684282c5a10d08888aaafb8426e
Sviatron <sviashe@gmail.com>
2025-12-14 17:00:10 +0200

    FINALLY

--- hw-3.js
+++ hw-3.js
@@ -3,3 +3,19 @@ console.log(result);
 console.log(typeof result);
 
 const email = "sviashe@gmail.com";
+const blacklist = "@";
+console.log(email.includes(blacklist));
+console.log(email.length);
+
+const my = "My";
+const name = "Name";
+const is = "Is";
+const fullName = `${my} ${name} ${is} Viktor`;
+console.log(fullName);
+
+let userName = "sviatik";
+let payment = "50 euros";
+let notification = alert(
+  `Thank you, ${userName}. Your payment of ${payment} has been transferred.`
+);
+console.log(notification);
