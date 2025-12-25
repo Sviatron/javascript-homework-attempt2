16ae4ede1bf743b605b107b29fb1283129d688e6
Sviatron <sviashe@gmail.com>
2025-12-25 19:16:40 +0200

    №4 done

--- hw-4.js
+++ hw-4.js
@@ -16,3 +16,24 @@ if (numA + numB > 10) {
 
 const blackListedWord = "JavaScript";
 let randomLine = "I love JavaScript <3";
+if (randomLine.includes(blackListedWord)) {
+  console.log("Текст містить слово JavaScript");
+} else {
+  console.log("Текст не містить слово JavaScript");
+}
+
+const numeristo = 16;
+if (numeristo >= 10 && numeristo <= 20) {
+  console.log("Число входить в діапазон від 10 до 20");
+} else {
+  console.log("Число не входить в діапазон від 10 до 20");
+}
+
+const email = "sviashe@gmail.com";
+const userName = "Sviatron";
+const password = "123456789";
+if (userName.length >= 3 && email.includes("@" && ".") && password >= 6) {
+  console.log("Перенаправлення на іншу сторінку");
+} else {
+  console.log("Помилка: неправильне заповнення");
+}
