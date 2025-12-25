af5de6d89d7729bb6c6a7785fb4a0d7e24bf38d6
Sviatron <sviashe@gmail.com>
2025-12-25 18:38:59 +0200

    halfway no. 4

---
+++ hw-4.js
@@ -0,0 +1,18 @@
+let lineA = "My name is Sviat";
+let lineB = "Yap yap yap";
+if (lineA.length + lineB.length >= 0) {
+  console.log("Обидва поля заповнені");
+} else {
+  console.log("Не всі поля заповнені");
+}
+
+const numA = 6;
+const numB = 7;
+if (numA + numB > 10) {
+  console.log("Сума більша за 10");
+} else {
+  console.log("Сума менша або дорівнює 10");
+}
+
+const blackListedWord = "JavaScript";
+let randomLine = "I love JavaScript <3";
