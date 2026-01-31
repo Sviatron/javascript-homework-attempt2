5cf1567b39d919bb443c582640b5bec6ecfc5a9b
Sviatron <sviashe@gmail.com>
2026-02-01 00:20:56 +0200

    no I do not (kinda)

--- hw-9.js
+++ hw-9.js
@@ -27,7 +27,56 @@ function findLongestWord(string) {
 }
 console.log(findLongestWord("Hi I have Hippopotomonstrosesquipedaliophobia"));
 
-function formatString(string) {
-  
+// function formatString(string) {
+//   if (string.length >= 40) {
+//     string.split(" ");
+//     string.splice(39, Infinity), string.splice(39, 0, "...");
+//   }
+// }
+// console.log(formatString("Hi I have Hippopotomonstrosesquipedaliophobia"));
+
+function checkForSpam(message) {
+  if (message.includes("spam " || "sale")) {
+    return !message.includes("spam " || "sale");
+  } else {
+    return message.includes("spam " || "sale");
+  }
 }
-console.log(formatString("Hi I have Hippopotomonstrosesquipedaliophobia"));
+console.log(checkForSpam("Hello this isn't or "));
+
+// let input;
+// const numbers = [];
+// let total = 0;
+// function calculation() {
+//   let prompt
+// }
+
+const logins = [
+  "sviat@gmail.com",
+  "sviashe@gmail.com",
+  "admin@gmail.com",
+  "testemail.@gmail.com",
+];
+
+function isLoginValid(login) {
+  return login.length >= 4 && login.length <= 16;
+}
+
+function isLoginUnique(allLogins, login) {
+  return !allLogins.includes(login);
+}
+
+function addLogin(allLogins, login) {
+  if (!isLoginValid(login)) {
+    return "Email not valid - doens't contain 4-16 symbols";
+  }
+  if (!isLoginUnique(allLogins, login)) {
+    return "Login not unique";
+  }
+  allLogins.push(login);
+  return "Login added to database";
+}
+
+console.log(addLogin(logins, "new@mail.com"));
+console.log(addLogin(logins, "test@gmail.com"));
+console.log(addLogin(logins, "bad"));
