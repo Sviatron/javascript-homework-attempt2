d6206d10696250b0437581249212e9fb705ce979
Sviatron <sviashe@gmail.com>
2026-03-17 00:33:49 +0200

    15 almost done (very hard)

--- hw-15.js
+++ hw-15.js
@@ -53,8 +53,31 @@ const calculateTotalBalance = (users) => {
 console.log(calculateTotalBalance(users)); // 20916
 
 const getUsersWithFriend = (users, friendName) => {
-  return users.reduce((friends, user) => user.friends === friendName, 0);
+  return users.reduce((acc, user) => {
+    if (user.friends.includes(friendName)) {
+      acc.push(user.name);
+    }
+    return acc;
+  }, []);
 };
-
 console.log(getUsersWithFriend(users, "Briana Decker")); // [ 'Sharlene Bush', 'Sheree Anthony' ]
 console.log(getUsersWithFriend(users, "Goldie Gentry")); // [ 'Elma Head', 'Sheree Anthony' ]
+
+const getNamesSortedByFriendsCount = (users) => {
+  return [...users]
+    .sort((a, b) => a.friends.length - b.friends.length)
+    .map((user) => user.name);
+};
+console.log(getNamesSortedByFriendsCount(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head',
+//  'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
+
+const getSortedUniqueSkills = (users) => {
+  return [...users]
+  .filter()
+};
+
+console.log(getSortedUniqueSkills(users));
+// [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa',
+//  'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem',
+//  'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
